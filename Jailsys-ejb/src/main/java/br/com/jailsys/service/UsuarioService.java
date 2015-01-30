c83aab4927ac8c0a3b916c57ed46b6795f251ef2
Siipe <siipecapoeira@gmail.com>
2015-01-30 14:03:02 -0200

    Criação do service do usuário
    Luiz Felippe

---
+++ Jailsys-ejb/src/main/java/br/com/jailsys/service/UsuarioService.java
@@ -0,0 +1,45 @@
+package br.com.jailsys.service;
+
+import java.util.List;
+
+import br.com.jailsys.model.EntidadeComum;
+
+public class UsuarioService implements AbstractService<EntidadeComum> {
+
+    @Override
+    public void salvar(EntidadeComum entidade) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public EntidadeComum salvarERetornar(EntidadeComum entidade) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void editar(EntidadeComum entidade) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<EntidadeComum> consultar() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void excluir(Long id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void buscar(Long id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
