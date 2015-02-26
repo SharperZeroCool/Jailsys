519d0b2394f760781a7474235d57f67aa85a71b6
SharperZeroCool <SharperZeroCool@gmail.com>
2015-02-26 11:13:12 -0300

    Adicionado envio de email no cadastro de usuario

--- Jailsys-web/src/main/java/br/com/jailsys/bean/UsuarioBean.java
+++ Jailsys-web/src/main/java/br/com/jailsys/bean/UsuarioBean.java
@@ -3,9 +3,16 @@ package br.com.jailsys.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import br.com.jailsys.bean.basic.AbstractBean;
 import br.com.jailsys.model.EntidadeComum;
@@ -15,6 +22,8 @@ import br.com.jailsys.service.UsuarioService;
 import br.com.jailsys.util.Constantes;
 import br.com.jailsys.util.FacesUtil;
 import br.com.jailsys.view.UsuarioView;
+
+import com.google.common.base.Strings;
 
 @ManagedBean
 @ViewScoped
@@ -27,6 +36,9 @@ public class UsuarioBean implements AbstractBean<EntidadeComum>, Serializable {
 
 	@Inject
 	private UsuarioView usuarioView;
+
+	@Resource(lookup = "java:jboss/mail/outlook")
+	private Session mailSession;
 
 	public List<Usuario> listar() {
 		if (usuarioView.getUsuarios().isEmpty()) {
@@ -49,6 +61,7 @@ public class UsuarioBean implements AbstractBean<EntidadeComum>, Serializable {
 	@Override
 	public String salvar() {
 		service.salvar(usuarioView.getUsuario());
+		enviarEmail(usuarioView.getUsuario());
 		FacesUtil.mostrarMensagemSucesso(Constantes.Usuario.MENSAGEM_CADASTRO);
 		this.atualizarView();
 		return Constantes.Usuario.TELA_CONSULTA;
@@ -88,6 +101,41 @@ public class UsuarioBean implements AbstractBean<EntidadeComum>, Serializable {
 
 	public void setUsuarioView(UsuarioView usuarioView) {
 		this.usuarioView = usuarioView;
+	}
+
+	private void enviarEmail(Usuario usuario) {
+		if (possuiEmail(usuario)) {
+			String to = usuario.getPessoa().getEmail();
+
+			mailSession.getProperties();
+
+			String from = mailSession.getProperty("mail.smtp.user");
+
+			try {
+				MimeMessage message = new MimeMessage(mailSession);
+
+				message.setFrom(new InternetAddress(from));
+
+				message.addRecipient(Message.RecipientType.TO,
+						new InternetAddress(to));
+
+				message.setSubject("Conta no Jailsys criada");
+
+				message.setContent(
+						"Parabéns " + usuario.getPessoa().getNome()
+								+ "!\nSua nova conta de usuário com o nome: "
+								+ usuario.getLogin()
+								+ " foi criada com sucesso!", "text/plain");
+
+				Transport.send(message);
+			} catch (MessagingException mex) {
+				mex.printStackTrace();
+			}
+		}
+	}
+
+	private static boolean possuiEmail(Usuario usuario) {
+		return !Strings.isNullOrEmpty(usuario.getPessoa().getEmail());
 	}
 
 }
