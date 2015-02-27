12c904165df4577da1eed484d99e26f3f597474a
SharperZeroCool <SharperZeroCool@gmail.com>
2015-02-27 13:34:37 -0300

    Padronizado botão de edição e salvar em cadastro de ambiente Script de alteração de campos BLOB para VARCHAR(10000)

---
+++ etc/bd/Scripts/2015-02-27-13-33 - Alteração Tabela Ambiente, Grupo e Crime.sql
@@ -0,0 +1,3 @@
+alter table GRUPO change descricao descricao VARCHAR(10000);
+alter table AMBIENTE change descricao descricao VARCHAR(10000);
+alter table CRIME change descricao descricao VARCHAR(10000);
