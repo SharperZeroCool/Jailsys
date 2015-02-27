bca37c4de5a40fce4f621ac64f4a11071d137b2e
luizgoiana <luizguilherme.goiana@gmail.com>
2015-02-27 10:29:02 -0300

    Refatoração

--- Jailsys-ejb/src/main/java/br/com/jailsys/service/FuncionarioService.java
+++ Jailsys-ejb/src/main/java/br/com/jailsys/service/FuncionarioService.java
@@ -1,7 +1,6 @@
 package br.com.jailsys.service;
 
 import java.io.Serializable;
-import java.util.Calendar;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -18,55 +17,19 @@ public class FuncionarioService implements AbstractService<EntidadeComum>,
     @Inject
     FuncionarioDAO funcionarioDao;
 
-    private final String PONTO = ".";
-
-    private final int REMOVE_PONTO = 1;
-
-    private final String HIFEN = "-";
-
-    private final String VAZIO = "";
-
     @Override
     public void salvar(EntidadeComum entidade) {
         Funcionario funcionario = (Funcionario) entidade;
-        funcionario.setCodigo(geradorDeCodigo(funcionario));
         funcionarioDao.salvar(funcionario);
-    }
-
-    /**
-     * Gera o codigo do funcionario no seguinte formato: ANO DE NASCIMENTO + 5
-     * ULTIMOS DIGITOS DO CPF + ANO DE ENTRADA + MES DE ENTRADA.
-     * 
-     * @param funcionario
-     * @return Codigo do funcionario no formato correto.
-     */
-    public String geradorDeCodigo(Funcionario funcionario) {
-        StringBuilder codigo = new StringBuilder();
-
-        // Pega o ano de nascimento do funcionario
-        Calendar calendario = Calendar.getInstance();
-        calendario.setTime(funcionario.getDataNasc());
-        codigo.append(calendario.get(Calendar.YEAR));
-
-        // Pega os ultimos 5 digitos do cpf do funcionario
-        codigo.append(funcionario.getCpf().substring(funcionario.getCpf().lastIndexOf(PONTO) + REMOVE_PONTO).replace(HIFEN, VAZIO));
-
-        // Pega o ano e mes de entrada do funcionario
-        calendario.setTime(funcionario.getDataEntrada());
-        codigo.append(calendario.get(Calendar.YEAR));
-        codigo.append(calendario.get(Calendar.MONTH));
-        return codigo.toString();
     }
 
     @Override
     public EntidadeComum salvarERetornar(EntidadeComum entidade) {
-        ((Funcionario) entidade).setCodigo(geradorDeCodigo((Funcionario) entidade));
         return funcionarioDao.salvarERetornar((Funcionario) entidade);
     }
 
     @Override
     public void editar(EntidadeComum entidade) {
-        ((Funcionario) entidade).setCodigo(geradorDeCodigo((Funcionario) entidade));
         funcionarioDao.editar((Funcionario) entidade);
     }
 
